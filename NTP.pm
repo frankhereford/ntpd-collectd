f40e38e117916036edb9bfa4a0fe025a386a18da
Frank Hereford <frank@frankhereford.com>
2016-07-21 22:47:21 -0500

    Add my favorite graph that shows the differences in values off peer servers

--- NTP.pm
+++ NTP.pm
@@ -2,11 +2,44 @@ package Collectd::Plugins::NTP;
 
 use strict;
 no strict "subs";
+use Tie::DNS;
 
 use Collectd qw (:all);
+tie my %dns, 'Tie::DNS';
 
 my $host = `hostname -f`;
 chomp $host;
+
+sub offset_read
+  {
+  my $cmd = 'ntpq -nc peers';
+  open(my $ntpq, "-|", $cmd); 
+  <$ntpq>;
+  <$ntpq>;
+  while (my $line = <$ntpq>)
+    {
+    my @data = split(/\s+/, $line);
+    $data[8] += 0;
+    next unless $data[8];
+    $data[0] =~ /[\+\-\*](\d+\.\d+\.\d+\.\d+)/;
+    my $ip = $1;
+    my $ntp_data =
+      {
+      plugin => 'NTP',
+      type_instance => ($dns{$ip} ? $dns{$ip} : $ip),
+      type => 'ntp_offset',
+      time => time,
+      interval => plugin_get_interval(),
+      host => $host,
+      values => [ $data[8] ],
+      };
+    plugin_dispatch_values ($ntp_data);
+    }
+  # the trick will be to offer a cron job to clean up old stale servers you have not seen in a while
+  close $ntpq;
+
+  return 1;
+  }
 
 sub ntpq_read
   {
@@ -74,6 +107,13 @@ sub ntpq_read
   return 1;
   }
 
-Collectd::plugin_register(Collectd::TYPE_READ, "NTP", "ntpq_read");
+sub read
+  {
+  ntpq_read();
+  offset_read(); 
+  return 1;
+  }
+
+Collectd::plugin_register(Collectd::TYPE_READ, "NTP", "read");
  
 1;
